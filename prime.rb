78d54074c33a00ffdd46663e9c777acc98d7d74f
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-22 05:22:47 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 # Add  code here!
 
 def prime?(n)
-  counter = n 
+  counter = n
   while counter >= 3
     counter = counter - 1
-    if n % counter == 0 
+    if n % counter == 0
       return "number is not prime"
     else
       puts "keep searching downward"
