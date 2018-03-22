37322b9cb2f28a71acc6d70c3f0cba05d0ca95d0
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-22 05:25:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+
+def prime?(n)
+  return false if n <= 1
+  counter = n
+  while counter >= 3
+    counter = counter - 1
+    if n % counter == 0
+      return false
+    else
+      puts "keep searching downward"
+    end
+  end
+  return true
+end
